f80998215c7bd8a2c191287866ab10e41f306a53
PatrickGHub <patrick@pkelly.co>
2025-02-27 20:03:30 +0000

    Initial commit

---
+++ src/DotnetAuCarScraper/DotnetAuCarScraperStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace DotnetAuCarScraper
+{
+    public class DotnetAuCarScraperStack : Stack
+    {
+        internal DotnetAuCarScraperStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
