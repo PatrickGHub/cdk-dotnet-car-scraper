7dcae97bac77db92a39b71fa5f4d8b1cfb470b44
PatrickGHub <patrick@pkelly.co>
2025-02-27 20:19:11 +0000

    Add audi lambda to stack

--- src/AudiScraper/src/AudiScraper/Function.cs
+++ src/AudiScraper/src/AudiScraper/Function.cs
@@ -7,13 +7,6 @@ namespace AudiScraper;
 
 public class Function
 {
-    
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input">The event for the Lambda function handler to process.</param>
-    /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
-    /// <returns></returns>
     public string FunctionHandler(string input, ILambdaContext context)
     {
         return input.ToUpper();
--- src/CdkStack/DotnetAuCarScraperStack.cs
+++ src/CdkStack/DotnetAuCarScraperStack.cs
@@ -1,4 +1,5 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.Lambda;
 using Constructs;
 
 namespace DotnetAuCarScraper
@@ -7,7 +8,32 @@ namespace DotnetAuCarScraper
     {
         internal DotnetAuCarScraperStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-            // The code that defines your stack goes here
+            var buildOption = new BundlingOptions()
+            {
+                Image = Runtime.DOTNET_8.BundlingImage,
+                User = "root",
+                OutputType = BundlingOutput.ARCHIVED,
+                Command = [
+                    "/bin/sh",
+                    "-c",
+                    " dotnet tool install -g Amazon.Lambda.Tools"+
+                    " && dotnet build"+
+                    " && dotnet lambda package --output-package /asset-output/function.zip"
+                ]
+            };
+
+            var audiScraperLambda = new Function(this, "AudiScraperFunction", new FunctionProps
+            {
+                FunctionName = "audi-scraper",
+                Runtime = Runtime.DOTNET_8,
+                Handler = "AudiScraper::AudiScraper.Function::FunctionHandler",
+                Code = Code.FromAsset("./src/AudiScraper/src/AudiScraper", new Amazon.CDK.AWS.S3.Assets.AssetOptions
+                {
+                    Bundling = buildOption
+                }),
+                MemorySize = 128,
+                Timeout = Duration.Seconds(30)
+            });
         }
     }
 }
