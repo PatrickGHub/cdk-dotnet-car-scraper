781a3f04ecf5f9bd5b56c663614abce096785ed7
PatrickGHub <patrick@pkelly.co>
2025-02-28 21:31:50 +0000

    Create lambda role with S3 put permission

--- src/CdkStack/DotnetAuCarScraperStack.cs
+++ src/CdkStack/DotnetAuCarScraperStack.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using Amazon.CDK;
+using Amazon.CDK.AWS.IAM;
 using Amazon.CDK.AWS.Lambda;
 using Amazon.CDK.AWS.S3;
 using Constructs;
@@ -9,6 +11,14 @@ namespace DotnetAuCarScraper
     {
         internal DotnetAuCarScraperStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
+            var audiScraperLambdaOutputBucket = new Bucket(this, "AudiScraperLambdaOutputBucket", new BucketProps
+            {
+                BlockPublicAccess = BlockPublicAccess.BLOCK_ALL,
+                BucketName = "audi-scraper-output-bucket",
+                Versioned = true,
+            }
+            );
+
             var buildOption = new BundlingOptions()
             {
                 Image = Runtime.DOTNET_8.BundlingImage,
@@ -23,6 +33,29 @@ namespace DotnetAuCarScraper
                 ]
             };
 
+            var audiScraperLambdaExecutionRole = new Role(this, "audiScraperLambdaExecutionRole", new RoleProps
+            {
+                AssumedBy = new ServicePrincipal("lambda.amazonaws.com"),
+                RoleName = "audi-scraper-execution-role",
+                InlinePolicies = new Dictionary<string, PolicyDocument>
+                {
+                    {
+                        "S3PutObjectPolicy",
+                        new PolicyDocument(new PolicyDocumentProps
+                        {
+                            Statements =
+                            [
+                                new PolicyStatement(new PolicyStatementProps
+                                {
+                                    Actions = ["s3:PutObject"],
+                                    Resources = [audiScraperLambdaOutputBucket.BucketArn]
+                                })
+                            ]
+                        })
+                    }
+                }
+            });
+
             var audiScraperLambda = new Function(this, "AudiScraperFunction", new FunctionProps
             {
                 FunctionName = "audi-scraper",
@@ -34,16 +67,9 @@ namespace DotnetAuCarScraper
                 Handler = "AudiScraper::AudiScraper.Function::FunctionHandler",
                 MemorySize = 128,
                 Runtime = Runtime.DOTNET_8,
+                Role = audiScraperLambdaExecutionRole,
                 Timeout = Duration.Seconds(30)
             });
-
-            var audiScraperLambdaOutputBucket = new Bucket(this, "AudiScraperLambdaOutputBucket", new BucketProps
-            {
-                BlockPublicAccess = BlockPublicAccess.BLOCK_ALL,
-                BucketName = "audi-scraper-output-bucket",
-                Versioned = true,
-            }
-            );
         }
     }
 }
