b9eb0c0a8e07ef33ab072d7eaa26a92598df817e
PatrickGHub <patrick@pkelly.co>
2025-03-02 11:42:28 +0000

    Add new lambda to read from audi S3

---
+++ src/AudiParser/src/AudiParser/Function.cs
@@ -0,0 +1,19 @@
+using Amazon.Lambda.Core;
+using AWS.Lambda.Powertools.Logging;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace AudiParser;
+
+public class Function
+{
+    public async Task<string> FunctionHandler(ILambdaContext context)
+    {
+        Logger.LogInformation("Lambda invoked");
+
+        var (s3Object, s3Error) = await S3Lib.GetS3Object("2025-02-28.json");
+        Logger.LogInformation(s3Object);
+        return null;
+    }
+}
