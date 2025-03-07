deb8208411522e499217e3ffa151e927adf23dc1
Patrick Kelly <patrick@pkelly.co>
2025-03-07 14:52:07 +0000

    Wrap DDB call in try catch

--- src/AudiParser/src/AudiParser/Lib/DynamoDB.cs
+++ src/AudiParser/src/AudiParser/Lib/DynamoDB.cs
@@ -1,18 +1,27 @@
 using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
 using AWS.Lambda.Powertools.Logging;
 
 public class DynamoDBLib
 {
-    static public async Task<long> BatchWriteItems(AmazonDynamoDBClient client, string listings)
+    static public async Task<(int? countOfStoredItems, Exception? error)> BatchWriteItems(AmazonDynamoDBClient client, List<Listings> listings)
     {
-        Logger.LogInformation("Writing batch of items to DynamoDB table");
-        var dynamoDbContext = new DynamoDBContext(client);
+        try
+        {
+            Logger.LogInformation("Writing batch of items to DynamoDB table");
+            var dynamoDbContext = new DynamoDBContext(client);
 
-        var listingsBatch = dynamoDbContext.CreateBatchWrite<Listing>();
-        listingsBatch.AddPutItems(listings);
+            var listingsBatch = dynamoDbContext.CreateBatchWrite<Listing>();
+            listingsBatch.AddPutItems(listings);
 
-        await listingsBatch.ExecuteAsync();
+            await listingsBatch.ExecuteAsync();
 
-        return listings.Count;
+            return (listings.Count, null);
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError("Failed to batch write items to DynamoDB");
+            return (null, ex);
+        }
     }
 }
