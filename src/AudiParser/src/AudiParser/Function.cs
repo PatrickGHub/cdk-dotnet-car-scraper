d465dfea3c2f3fe7509982e3e2a96e256791b45a
Patrick Kelly <patrick@pkelly.co>
2025-03-07 14:52:19 +0000

    Begin handling json for DDB

--- src/AudiParser/src/AudiParser/Function.cs
+++ src/AudiParser/src/AudiParser/Function.cs
@@ -1,6 +1,9 @@
+using Amazon.DynamoDBv2;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.S3Events;
 using AWS.Lambda.Powertools.Logging;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -13,10 +16,20 @@ public class Function
     {
         Logger.LogInformation(bucketEvent);
         Logger.LogInformation("Lambda invoked");
-
+        var dynamoDbClient = new AmazonDynamoDBClient();
         var bucketEventObject = bucketEvent.Records[0].S3.Object.Key;
 
         var (s3Object, s3Error) = await S3Lib.GetS3Object(bucketEventObject);
+
+        var allListings = JsonSerializer.Deserialize<List<Listing>>(
+            s3Object,
+            new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            }
+        );
+
+        var batchWriteResult = await DynamoDBLib.BatchWriteItems(dynamoDbClient, allListings);
         return null;
     }
 }
